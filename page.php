f64967de2fd3f22804287233bc14005a8f739cf4
Freddie Mixell <fmixell@gmail.com>
2021-05-11 12:43:37 -0400

    column layout for sidebar

--- page.php
+++ page.php
@@ -14,25 +14,28 @@
 
 get_header();
 ?>
+	<div class="columns">
+		<main id="primary" class="site-main column">
+			<section class="section">
+				<div class="content">
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-	<main id="primary" class="site-main">
+						get_template_part( 'template-parts/content', 'page' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-			get_template_part( 'template-parts/content', 'page' );
+					endwhile; // End of the loop.
+					?>
+				</div>
+			</section>
+		</main><!-- #main -->
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+<?php get_sidebar(); ?>
+	</div> <!-- .columns -->
 <?php
-get_sidebar();
 get_footer();
--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<aside id="secondary" class="column is-one-fifth">
+	<div class="content">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
 </aside><!-- #secondary -->
