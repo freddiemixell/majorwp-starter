1112be9f5d8200a52519882d720893db5d792814
Freddie Mixell <fmixell@gmail.com>
2021-05-10 21:06:37 -0400

    custom front-page.php template with hero

---
+++ front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Front Page Template
+ *
+ * @package MajorWP_Starter
+ */
+
+get_header();
+?>
+
+    <div class="hero is-large">
+        <div class="hero-body has-text-centered">
+            <h1 class="title is-spaced is-1">Websites like this?</h1>
+            <p class="subtitle is-4">It's Major.</p>
+
+            <div class="buttons is-centered">
+                <a class="button is-primary">Get Started</a>
+                <a class="button is-dark is-outlined">Docs</a>
+            </div>
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
