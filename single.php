54ebff5e23bb726ecf515ddaf7944d60219bf699
Freddie Mixell <fmixell@gmail.com>
2021-05-12 11:14:34 -0400

    single post columns

--- single.php
+++ single.php
@@ -9,32 +9,32 @@
 
 get_header();
 ?>
+	<div class="columns">
+		<main id="primary" class="site-main column content">
 
-	<main id="primary" class="site-main">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'major-wp' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'major-wp' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'major-wp' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'major-wp' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+		</main><!-- #main -->
+<?php get_sidebar(); ?>
+	</div><!-- .columns -->
 <?php
-get_sidebar();
 get_footer();
